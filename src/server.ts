ce8dd08cb63a96743229baeecd135678f556a613
zagozagoo <vivizago4884@gmail.com>
2024-08-13 11:07:37 -0300

    aaaaaaaa

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("rodandoo")
+import fastify from 'fastify'
+// console.log("rodandoo")
+const server = fastify();
+
+server.get('/', () => {
+    return "Ola mundo"
+})
+
+server.listen({port : 3333}).then(() =>
+    console.log("SERVER RUNNING")
+)
